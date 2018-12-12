823683ffc5baa9bbfd58ff93c087479aba289d89
Jose Stricklin <josestricklin@El-Diablo.local>
2018-12-11 21:58:29 -0700

    update redis functions

--- cache.js
+++ cache.js
@@ -4,16 +4,17 @@ bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 const client = redis.createClient();
 
+client.keysAsync('*').then( res => console.log('start keys', res) ) // check keys in redis on save
+
 module.exports = {
     getCachedPlayerData: (playerName) => {
-        // console.log('redis player data start', data.relationships.matches.data);
-        // client.getAsync(playerName).then( res => console.log('get keys', JSON.parse(res)))
+        client.keysAsync('*').then( res => console.log('get data all keys', res) )
         return client.getAsync(playerName).then( res => JSON.parse(res))
     },
     cachePlayerData: (playerName, data) => {
         console.log('Caching player data', playerName)
         client.setAsync(playerName, JSON.stringify(data), 'EX', 180).then(res => console.log('set', res))
         client.getAsync(playerName).then( res => console.log('get after set', JSON.parse(res)))
-        // client.keysAsync('*').then( res => console.log('post set keys', res) )
+        client.keysAsync('*').then( res => console.log('post set keys', res) )
     }
 }
